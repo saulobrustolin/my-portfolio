d59476a5fe038f874fd884ec33a988c9b55380f2
saulo <saulobrustolin@icloud.com>
2025-05-26 23:41:36 -0300

    back release

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Content from "./modules/Content";
 
 export default function Home() {
   return(
-    <div className="py-8 px-12 h-screen w-screen flex flex-col items-end">
+    <div className="py-8 px-12 h-screen w-screen">
     <Header/>
     <Content/>
     </div>
