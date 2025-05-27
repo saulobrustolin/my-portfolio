8cb800f277c6098bdaebe0654ca37b35dfad6d8c
saulo <saulobrustolin@icloud.com>
2025-05-27 00:00:34 -0300

    update responsive mobile

--- app/modules/Header.tsx
+++ app/modules/Header.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 export default function Header() {
     return(
-        <header className="fixed">
-            <ul className="flex gap-8 justify-end">
+        <header className="fixed left-1/2 sm:left-[70%] md:left-[75%] lg:left-[80%]">
+            <ul className="flex gap-8">
                 <li>
-                    <Link className="anchor-direction" href="https://www.linkedin.com/in/brustolinsaulo/" target="blank">
+                    <Link className="anchor-direction text-sm sm:text-base" href="https://www.linkedin.com/in/brustolinsaulo/" target="blank">
                         Linkedin
                     </Link>
                 </li>
 
                 <li>
-                    <Link className="anchor-direction" href="https://github.com/saulobrustolin" target="blank">
+                    <Link className="anchor-direction text-sm sm:text-base" href="https://github.com/saulobrustolin" target="blank">
                         Github
                     </Link>
                 </li>
